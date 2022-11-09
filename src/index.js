4f3052d9d03e5da9434fcf044d3947bd49e8bc88
BorbaBruno <brunodeborba@outlook.com>
2022-11-09 15:58:30 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { request, response } = require('express')
+const express = require('express')
+
+const app = express()
+
+app.post('/account', (request, response) => {
+  const cpf = request.body;
+  
+})
