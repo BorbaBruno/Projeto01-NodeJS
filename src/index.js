cba7ddc79e32d357826b9896bc578fedb691a47d
BorbaBruno <brunodeborba@outlook.com>
2022-11-09 17:13:16 -0300

    following README requirements

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-const { request, response } = require('express')
+const { v4: uuidv4 } = require('uuid')
 const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
+const customers = []
+
+/*
+ CPF = String
+ NAME = String
+ ID = UUID / Identificador Único Universal
+ Statement = []
+ */
+
 app.post('/account', (request, response) => {
-  const cpf = request.body;
-  
+  const { cpf, name } = request.body
+
+  const customerAlreadyExists = customers.some(
+    (customers) => customers.cpf === cpf
+  )
+
+  if(customerAlreadyExists) {
+    return response.status(400).json({error: "Customer Already Exists!"});
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  })
+
+  return response.status(201).send()
 })
+
+app.listen(3333)
